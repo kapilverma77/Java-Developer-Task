3a5ead91d8d2d597c78c34f7d60cf13e289300ab
kapilverma77 <69675559+kapilverma77@users.noreply.github.com>
2022-01-08 23:22:22 +0530

    Update MyClass1.java

--- MyClass1.java
+++ MyClass1.java
@@ -1,6 +1,12 @@
+/* Ques 1 
+    Take an input roman no from user & convert it into its corresponding numeric value. 
+    Eg. Input: X
+        Output: 10
+        Input: IX
+        Output: 9
+*/
 import java.util.*;
 public class MyClass {
-
     public static int romanToInt(String s)
     {
         int n = s.length();
@@ -12,28 +18,23 @@ public class MyClass {
         mp.put('C',100);
         mp.put('D',500);
         mp.put('M',1000);
-
         int sum = 0;
-        
-        for(int i=0;i<n-1;i++){
-            
-            if(mp.get(s.charAt(i)) >= mp.get(s.charAt(i+1))){
-                
+        for(int i=0;i<n-1;i++)
+        {
+            if(mp.get(s.charAt(i)) >= mp.get(s.charAt(i+1)))
+            {
                 sum += mp.get(s.charAt(i));
             }
-            
-
-      else{
+            else
+            {
                sum  -= mp.get(s.charAt(i));
             }
         }
-        
         sum += mp.get(s.charAt(n-1));
         return sum;
     }
-
-    public static void main(String args[]) {
-
+    public static void main(String args[]) 
+    {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter your string : ");
       String s = sc.nextLine();
